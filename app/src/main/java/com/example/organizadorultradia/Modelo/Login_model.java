4acbeec88876e04c60bcfad2e7db59947e77c71e
Ansalemon <47870889+Ansalemon@users.noreply.github.com>
2019-03-01 12:39:30 -0500

    Interface implementada, constructor de model conectado con el presentador.

--- app/src/main/java/com/example/organizadorultradia/Modelo/Login_model.java
+++ app/src/main/java/com/example/organizadorultradia/Modelo/Login_model.java
@@ -1,4 +1,23 @@
 package com.example.organizadorultradia.Modelo;
 
-public class Login_model {
+import com.example.organizadorultradia.Interface.Interface;
+import com.example.organizadorultradia.Presentador.presentador;
+
+public class Login_model implements Interface.Model {
+    private Interface.Presenter Presenter;
+
+    public Login_model(Interface.Presenter presenter) {
+        this.Presenter=presenter;
+    }
+
+
+    @Override
+    public void RegistrarLogin(String Email, int Pass) {
+
+    }
+
+    @Override
+    public void RegistrarActividad(String actividad, int duracion, String descripcion) {
+
+    }
 }
--- app/src/main/java/com/example/organizadorultradia/Presentador/presentador.java
+++ app/src/main/java/com/example/organizadorultradia/Presentador/presentador.java
@@ -3,12 +3,21 @@ package com.example.organizadorultradia.Presentador;
 import android.view.View;
 import android.widget.EditText;
 import com.example.organizadorultradia.Interface.Interface;
+import com.example.organizadorultradia.Modelo.Login_model;
 
-public class presentador {
+public class presentador implements Interface.Presenter {
 private Interface.View view;
 private Interface.Model model;
     public void presentador(Interface.View view, Interface.Model model){
       this.view = view;
-      this.model=model;
+      model=new Login_model(this);
+    }
+    @Override
+    public void RegistrarLogin(String Email, int Pass) {
+
+    }
+    @Override
+    public void RegistrarActividad(String actividad, int duracion, String descripcion) {
+
     }
 }
