d06a3426eed66e3a3259f5b6099961d02c28f6ea
felipe <juanf.alfonso@correo.usa.edu.co>
2019-04-24 17:55:56 -0500

    h

---
+++ app/src/main/java/com/example/organizadorultradia/clases/Informacion.java
@@ -0,0 +1,8 @@
+package com.example.organizadorultradia.clases;
+
+public class Informacion {
+    public String fecha;
+    public int hora;
+    public String titulo, descripcion;
+    public int duracion;
+}
