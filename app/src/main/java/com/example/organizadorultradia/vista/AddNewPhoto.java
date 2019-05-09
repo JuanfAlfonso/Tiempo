5c03023016fa691169167cf15bd72c680c3409b6
Ansalemon <santilegui4@gmail.com>
2019-05-09 12:10:17 -0500

    camara funcional, con multimedia, para el registro, cambio de iconos, cambio de tema, posiblemente lo que vayamos apresentar en la primera iteracion 3 corte

--- app/src/main/java/com/example/organizadorultradia/vista/AddNewPhoto.java
+++ app/src/main/java/com/example/organizadorultradia/vista/AddNewPhoto.java
@@ -1,13 +1,19 @@
 package com.example.organizadorultradia.vista;
 
 
-import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,110 +24,205 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.organizadorultradia.R;
-import com.example.organizadorultradia.clases.PhotoUtils;
 
 import java.io.File;
+import java.net.URI;
 
-public class AddNewPhoto extends AppCompatActivity{
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.support.v4.content.FileProvider.getUriForFile;
 
-    private AlertDialog _photoDialog;
-    private Uri mImageUri;
-    private static final int ACTIVITY_SELECT_IMAGE = 1020,
-            ACTIVITY_SELECT_FROM_CAMERA = 1040, ACTIVITY_SHARE = 1030;
-    private PhotoUtils photoUtils;
-    private Button photoButton;
-    private ImageView photoViewer;
-    private boolean fromShare;
-    public void onCreate(Bundle savedInstanceState) {
+
+public class AddNewPhoto extends AppCompatActivity {
+
+    private final String CARPETA_RAIZ="misImagenesPrueba/";
+    private final String RUTA_IMAGEN=CARPETA_RAIZ+"misFotos";
+
+    final int COD_SELECCIONA=10;
+    final int COD_FOTO=20;
+
+    Button botonCargar;
+    ImageView imagen;
+    String path;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_photo);
-        photoUtils = new PhotoUtils(this);
-        // Get intent, action and MIME type
-        Intent intent = getIntent();
-        String action = intent.getAction();
-        String type = intent.getType();
-        if (Intent.ACTION_SEND.equals(action) && type != null) {
-            if ("text/plain".equals(type)) {
-                fromShare = true;
-            } else if (type.startsWith("image/")) {
-                fromShare = true;
-                mImageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-                getImage(mImageUri);
+
+        imagen=findViewById(R.id.photoViewer);
+        botonCargar=findViewById(R.id.photoButton);
+
+        if(validaPermisos()){
+            botonCargar.setEnabled(true);
+        }else{
+            botonCargar.setEnabled(false);
+        }
+
+    }
+
+    private boolean validaPermisos() {
+
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+            return true;
+        }
+
+        if((checkSelfPermission(CAMERA)==PackageManager.PERMISSION_GRANTED)&&
+                (checkSelfPermission(WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED)){
+            return true;
+        }
+
+        if((shouldShowRequestPermissionRationale(CAMERA)) ||
+                (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE))){
+            cargarDialogoRecomendacion();
+        }else{
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA},100);
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==100){
+            if(grantResults.length==2 && grantResults[0]==PackageManager.PERMISSION_GRANTED
+                    && grantResults[1]==PackageManager.PERMISSION_GRANTED){
+                botonCargar.setEnabled(true);
+            }else{
+                solicitarPermisosManual();
             }
         }
-        photoButton =  findViewById(R.id.photoButton);
-        photoViewer = findViewById(R.id.photoViewer);
-        getPhotoDialog();
-        setPhotoButton();
 
     }
-    public void getImage(Uri uri) {
-        Bitmap bounds = photoUtils.getImage(uri);
-        if (bounds != null) {
-            setImageBitmap(bounds);
-        } else {
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
 
-        }
-    }
-    private void setImageBitmap(Bitmap bitmap){
-        photoViewer.setImageBitmap(bitmap);
-    }
-
-    private AlertDialog getPhotoDialog() {
-        if (_photoDialog == null) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle(R.string.photo_source);
-            builder.setPositiveButton(R.string.camera, new DialogInterface.OnClickListener() {
-
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Intent intent = new Intent(
-                            "android.media.action.IMAGE_CAPTURE");
-                    File photo = null;
-                    try {
-                        // place where to store camera taken picture
-                        photo = PhotoUtils.createTemporaryFile("picture", ".jpg", AddNewPhoto.this);
-                        photo.delete();
-                    } catch (Exception e) {
-                        Log.v(getClass().getSimpleName(),
-                                "Can't create file to take picture!");
-                    }
-                    mImageUri = Uri.fromFile(photo);
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-                    startActivityForResult(intent, ACTIVITY_SELECT_FROM_CAMERA);
-
+    private void solicitarPermisosManual() {
+        final CharSequence[] opciones={"si","no"};
+        final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(AddNewPhoto.this);
+        alertOpciones.setTitle("¿Desea configurar los permisos de forma manual?");
+        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("si")){
+                    Intent intent=new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri=Uri.fromParts("package",getPackageName(),null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Los permisos no fueron aceptados",Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
                 }
-
-            });
-            builder.setNegativeButton(R.string.gallery, new DialogInterface.OnClickListener() {
-
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                    galleryIntent.setType("image/*");
-                    startActivityForResult(galleryIntent, ACTIVITY_SELECT_IMAGE);
-                }
-
-
-            });
-            _photoDialog = builder.create();
-
-        }
-        return _photoDialog;
-
-    }
-    private void setPhotoButton(){
-        photoButton.setOnClickListener(new View.OnClickListener() {
-            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-            public void onClick(View v){
-                if(!getPhotoDialog().isShowing() && !isFinishing())
-                    getPhotoDialog().show();
-
             }
         });
+        alertOpciones.show();
     }
 
+    private void cargarDialogoRecomendacion() {
+        AlertDialog.Builder dialogo=new AlertDialog.Builder(AddNewPhoto.this);
+        dialogo.setTitle("Permisos Desactivados");
+        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
+
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA},100);
+            }
+        });
+        dialogo.show();
+    }
+
+    public void onclick(View view) {
+        cargarImagen();
+    }
+
+    private void cargarImagen() {
+
+        final CharSequence[] opciones={"Tomar Foto","Cargar Imagen","Cancelar"};
+        final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(AddNewPhoto.this);
+        alertOpciones.setTitle("Seleccione una Opción");
+        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("Tomar Foto")){
+                    tomarFotografia();
+                }else{
+                    if (opciones[i].equals("Cargar Imagen")){
+                        Intent intent=new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),COD_SELECCIONA);
+                    }else{
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        alertOpciones.show();
+
+    }
+
+    private void tomarFotografia() {
+        File fileImagen=new File(Environment.getExternalStorageDirectory(),RUTA_IMAGEN);
+        boolean isCreada=fileImagen.exists();
+        String nombreImagen="";
+        if(isCreada==false){
+            isCreada=fileImagen.mkdirs();
+        }
+
+        if(isCreada==true){
+            nombreImagen=(System.currentTimeMillis()/1000)+".jpg";
+        }
 
 
+        path=Environment.getExternalStorageDirectory()+
+                File.separator+RUTA_IMAGEN+File.separator+nombreImagen;
+
+        File imagen=new File(path);
+
+        Intent intent=null;
+        intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ////
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N)
+        {
+            String authorities=getApplicationContext().getPackageName()+".provider";
+            Uri imageUri=FileProvider.getUriForFile(this,authorities,imagen);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        }else
+        {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
+        }
+        startActivityForResult(intent,COD_FOTO);
+
+        ////
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK){
+
+            switch (requestCode){
+                case COD_SELECCIONA:
+                    Uri miPath=data.getData();
+                    imagen.setImageURI(miPath);
+                    break;
+
+                case COD_FOTO:
+                    MediaScannerConnection.scanFile(this, new String[]{path}, null,
+                            new MediaScannerConnection.OnScanCompletedListener() {
+                                @Override
+                                public void onScanCompleted(String path, Uri uri) {
+                                    Log.i("Ruta de almacenamiento","Path: "+path);
+                                }
+                            });
+
+                    Bitmap bitmap= BitmapFactory.decodeFile(path);
+                    imagen.setImageBitmap(bitmap);
+
+                    break;
+            }
+
+
+        }
+    }
 }
