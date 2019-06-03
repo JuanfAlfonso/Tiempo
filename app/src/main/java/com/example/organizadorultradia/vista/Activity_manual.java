1e40a9f8a6e14fe7070bd07c331808687e487c1d
felipe <juanf.alfonso@correo.usa.edu.co>
2019-06-02 20:40:17 -0500

    fecha añadida

--- app/src/main/java/com/example/organizadorultradia/vista/Activity_manual.java
+++ app/src/main/java/com/example/organizadorultradia/vista/Activity_manual.java
@@ -1,15 +1,21 @@
 package com.example.organizadorultradia.vista;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.organizadorultradia.Presenter.PresentadorManual;
 import com.example.organizadorultradia.R;
 
-public class Activity_manual extends AppCompatActivity {
+import java.util.Calendar;
+
+public class Activity_manual extends AppCompatActivity implements View.OnClickListener {
+
     public TextView tit;
     public TextView dur;
     public TextView des;
@@ -18,10 +24,20 @@ public class Activity_manual extends AppCompatActivity {
     String Actividad;
     String Duracion;
     String Descripcion;
+
+    ImageButton fecha;
+    TextView fechita;
+    public int dia, mes, ano;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registromanual);
+
+        fecha = (ImageButton) findViewById(R.id.fechita);
+        fecha.setOnClickListener(this);
+        fechita = (TextView) findViewById(R.id.Fecha);
+
         setUpActivity();
     }
 
@@ -53,4 +69,21 @@ public class Activity_manual extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        if(v==fecha){
+            final Calendar c= Calendar.getInstance();
+            dia=c.get(Calendar.DAY_OF_MONTH);
+            mes=c.get(Calendar.MONTH);
+            ano=c.get(Calendar.YEAR);
+            DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    fechita.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                }
+            }
+            ,dia,mes,ano);
+            dpd.show();
+        }
+    }
 }
